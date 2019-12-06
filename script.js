de1bf2477011823a97d4c00f29a10c033ceacfdf
kgauzampe <kgauza.mpe@gmail.com>
2019-12-06 09:51:17 +0200

    second commit

---
+++ script.js
@@ -0,0 +1,51 @@
+const cards = document.querySelectorAll('.memory-card');
+
+let cardFlipped = false;
+let firstCard, secondCard;
+
+function flipCard(){
+  this.classList.add('flip');
+
+  if (!cardFlipped){
+    cardFlipped = true; // First click
+    firstCard = this;
+    return;
+} else{
+    cardFlipped = false; // second click
+    secondCard = this;
+
+    // checking if cards match
+    if(firstCard.childNodes[3].src == secondCard.childNodes[3].src){
+        firstCard.removeEventListener('click',flipCard); // if its a match
+        secondCard.removeEventListener('click',flipCard);
+
+    } else{
+        // if its not a match
+            setTimeout(() => {
+            firstCard.classList.remove('flip');  // flipback cards that dont match
+            secondCard.classList.remove('flip');
+        }, 1500);
+
+       console.log('flipback');
+    }
+}
+
+
+ 
+}
+
+(function shuffle() {
+
+    cards.forEach(card => {
+  
+      let randomPos = Math.floor(Math.random() * 12); // shuffle the cards
+  
+      card.style.order = randomPos;
+  
+    });
+  
+})();
+
+cards.forEach(card => card.addEventListener('click', flipCard));  
+
+
